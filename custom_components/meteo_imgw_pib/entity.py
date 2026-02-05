ea7b5d0915c87c1ef5e20e449b90a5bdf799f87e
Maciej Bieniek <bieniu@users.noreply.github.com>
2026-02-05 09:10:51 +0100

    Fix configuration_url (#224)

--- custom_components/meteo_imgw_pib/entity.py
+++ custom_components/meteo_imgw_pib/entity.py
@@ -1,9 +1,10 @@
 """Define the IMGW-PIB entity."""
 
+from homeassistant.const import ATTR_CONFIGURATION_URL
 from homeassistant.helpers.device_registry import DeviceEntryType, DeviceInfo
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-from .const import ATTRIBUTION, DOMAIN
+from .const import ATTRIBUTION, CONFIGURATION_URL, DOMAIN
 from .coordinator import MeteoImgwPibDataUpdateCoordinator
 
 
@@ -26,4 +27,12 @@ class MeteoImgwPibEntity(CoordinatorEntity[MeteoImgwPibDataUpdateCoordinator]):
             manufacturer="IMGW-PIB",
             name=f"{coordinator.imgwpib.weather_stations[coordinator.station_id]}",
         )
+        latitude = coordinator.data.latitude
+        longitude = coordinator.data.longitude
+        if latitude is not None and longitude is not None:
+            device_info[ATTR_CONFIGURATION_URL] = CONFIGURATION_URL.format(
+                latitude=latitude,
+                longitude=longitude,
+            )
+
         self._attr_device_info = device_info
